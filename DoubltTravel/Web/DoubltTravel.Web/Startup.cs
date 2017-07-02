a223ad11325a383d5d2bd268bf7ed60e263e9d6d
Kamen <k.todorov@ultraplay.co>
2017-07-02 20:19:06 +0300

    added site

---
+++ DoubltTravel/Web/DoubltTravel.Web/Startup.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoubltTravel.Data;
+using DoubltTravel.Data.AssistenceInfos;
+using DoubltTravel.Data.Countrues;
+using DoubltTravel.Data.CountryInfo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
+
+namespace DoubltTravel.Web
+{
+    public class Startup
+    {
+        private Container container = new Container();
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton<IControllerActivator>(
+             new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(container));
+
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));InvalidOperationException: No service for type 'Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration' has been registered.
+            //loggerFactory.AddDebug();
+            try
+            {
+                InitializeContainer(app);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            //// Add application services. For instance:
+            //container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Scoped);
+            container.Register<IConnectionStringProvider, HardcodedConnectionStringProvider>();
+            container.Register<ICountryInfoRepository, CountryInfoRepository>();
+            container.Register<IAssistenceInfoRepository, AssistenceInfoRepository>();
+            container.Register<ICountryRepository, CountryRepository>();
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            container.RegisterSingleton(app.ApplicationServices.GetService<ILoggerFactory>());
+
+            // The following registers a Func<T> delegate that can be injected as singleton,
+            // and on invocation resolves a MVC IViewBufferScope service for that request.
+            container.RegisterSingleton<Func<IViewBufferScope>>(
+                () => app.GetRequestService<IViewBufferScope>());
+
+            container.Verify();
+            // NOTE: Do prevent cross-wired instances as much as possible.
+            // See: https://simpleinjector.org/blog/2016/07/
+        }
+    }
+}
